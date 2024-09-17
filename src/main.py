10b049796c34f4662e039781f68c07e4cc2005ec
hoyath <hoyathali@gmail.com>
2024-09-17 21:23:44 +0530

    experian code

--- src/main.py
+++ src/main.py
@@ -6,35 +6,41 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
 
 def scrape_and_parse(sub, page_num):
-    SCRAPE_BASE_URL = "https://www.bankrate.com/"
+    SCRAPE_BASE_URL = sub["base"]
     SCRAPE_URL = SCRAPE_BASE_URL + sub["url"] + str(page_num)
-    
     scraper = Scraper(SCRAPE_URL)
     raw_data = scraper.scrape()
-    
     parser = Parser(SCRAPE_BASE_URL, raw_data, sub['tag'])
-    parsed_data = parser.bankrate_parse()
     
+    if "bankrate" in SCRAPE_BASE_URL:
+        parsed_data = parser.bankrate_parse()
+
+    if "wsj" in SCRAPE_BASE_URL:
+        parsed_data = parser.wsj_parse()  
+    
+    if "experian" in SCRAPE_BASE_URL:
+        parsed_data = parser.experian_parse()
+
     path = f"data/processed/{sub['tag']}.csv"
     save_data(parsed_data, path)
     return f"Completed scraping and parsing for page {page_num} of {sub['tag']}"
 
 def main():
-    # src/config.py
     SUB_URL = [
-        #{"url": "banking/?feed=banking-stories&page=", "pages": 127, "tag": "banking"},
-        #{"url": "mortgages/?feed=mortgage-stories&page=", "pages": 77, "tag": "mortgages"},
-        {"url": "investing/?page=", "pages": 50,"tag": "investing"},
-        {"url": "credit-cards/?page=", "pages": 180,"tag": "credit-cards"},
-        {"url": "loans/?page=", "pages": 32,"tag": "loans"},
-        #{"url": "home-equity/?feed=home-equity-stories&page=", "pages": 11, "tag": "home-equity"},
-        #{"url": "insurance/?feed=insurance-stories&page=", "pages": 108, "tag": "insurance"},
+        # {"base":"https://www.bankrate.com/", "url": "banking/?feed=banking-stories&page=", "pages": 127, "tag": "banking"},
+        # {"base":"https://www.bankrate.com/", "url": "mortgages/?feed=mortgage-stories&page=", "pages": 77, "tag": "mortgages"},
+        # {"base":"https://www.bankrate.com/", "url": "investing/?page=", "pages": 50,"tag": "investing"},
+        # {"base":"https://www.bankrate.com/", "url": "credit-cards/?page=", "pages": 180,"tag": "credit-cards"},
+        # {"base":"https://www.bankrate.com/", "url": "loans/?page=", "pages": 32,"tag": "loans"},
+        # {"base":"https://www.bankrate.com/", "url": "home-equity/?feed=home-equity-stories&page=", "pages": 11, "tag": "home-equity"},
+        # {"base":"https://www.bankrate.com/", "url": "insurance/?feed=insurance-stories&page=", "pages": 108, "tag": "insurance"},
+
+        {"base":"https://www.experian.com/blogs/ask-experian/page/", "url": "", "pages": 318, "tag": "finance"},
+       
     ]
 
-    HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
-
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
     log_message("Starting the web scraping project...")
 
     for sub in SUB_URL:
--- src/parser.py
+++ src/parser.py
@@ -2,6 +2,8 @@
 from bs4 import BeautifulSoup
 from utils import log_message
 from tqdm import tqdm
+import re
+
 class Parser:
     def __init__(self, baseurl, raw_data, tag):
         self.raw_data = raw_data
@@ -62,8 +64,82 @@ class Parser:
                 data.append({
                     'title': title,
                     'tag': self.tag,
-                    'link': link,
+                    'link': self.baseurl+link,
                     'description': description,
                     'date': date
                 })
             return data
+    def wsj_parse(self):
+        log_message("Parsing the scraped data from wsj...")
+        
+        if self.raw_data is None:
+            log_message("No data to parse.")
+            return {}
+
+        soup = BeautifulSoup(self.raw_data, 'html.parser')
+
+        articles = soup.select('#latest-articles .t-card__content')
+
+        data = []
+
+        for article in articles:
+            title = article.find('h3', class_='t-card__title').get_text(strip=True)
+            link = article.find('a', class_='t-card__link')['href']
+            description = article.find('div', class_='t-card__body').get_text(strip=True)
+            date = article.find('div', class_='t-byline__meta').get_text(strip=True)
+            
+            # Append the extracted information to the data list
+            data.append({
+                'title': title,
+                'tag': self.tag,
+                'link': self.baseurl+link,
+                'description': description,
+                'date': date
+            })
+
+        return data
+    
+    def experian_parse(self):
+        log_message("Parsing the scraped data from experian...")
+
+        if self.raw_data is None:
+            log_message("No data to parse.")
+            return {}
+
+        soup = BeautifulSoup(self.raw_data, 'html.parser')
+
+        # Find the container with the id artBlk
+        articles = soup.select('#artBlk .post.flx.flx-yc.flx-jc-xc')
+
+        data = []
+
+        for article in articles:
+            link_tag = article.find('a')
+            if not link_tag:
+                continue
+            
+            # Extract article details
+            link = link_tag['href']
+            title = link_tag.find('h5').get_text(strip=True)
+            description_tag = link_tag.find('span', class_='hidden-xs hidden-sm')
+            description = description_tag.get_text(strip=True) if description_tag else ""
+            date_tag = link_tag.find('p', class_='date')
+
+            date_tag = link_tag.find('p', class_='date')
+            if date_tag:
+                # Use regex to extract the date portion before "•"
+                date = re.search(r"^(.*?)(?:\s+•|$)", date_tag.get_text(strip=True)).group(1)
+            else:
+                date = ""
+            # Append the extracted information to the data list
+            data.append({
+                'title': title,
+                'tag': self.tag,
+                'link': link,
+                'description': description,
+                'date': date
+            })
+
+        return data
+
+            
